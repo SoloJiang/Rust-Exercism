de89ae74c308748775e0c9800712b18148b0d7aa
fushen.jzw <fushen.jzw@alibaba-inc.com>
2021-01-31 23:41:44 +0800

    refactor: beer song

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,43 +1,29 @@
-pub fn verse(n: u32) -> String {
-    let mut str: String = String::new();
-    let m: String = n.to_string();
-    if n == 0 {
-      str += "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
-    } else {
-      str += &m;
-      str += &get_bottle(n > 1);
-      str += " of beer on the wall, ";
-      str += &m;
-      str += &get_bottle(n > 1);
-      str += " of beer.\nTake ";
-      if n > 1 {
-        str += "one";
-      } else {
-        str += "it";
-      }
-      str += " down and pass it around, ";
-      if n == 1 {
-        str += "no more bottles";
-      } else {
-        str += &(n - 1).to_string();
-        str += &get_bottle(n - 2 > 0);
-      }
-      str += " of beer on the wall.\n";
-    }
-    str
+fn capitalized(target: String) -> String {
+  let mut target_chars = target.chars();
+  match target_chars.next() {
+    None => String::new(),
+    Some(f) => f.to_uppercase().chain(target_chars).collect(),
+  }
 }
 
-fn get_bottle(x: bool) -> String {
-  if x { String::from(" bottles") } else { String::from(" bottle") }
+pub fn verse(number: u32) -> String {
+  let b = |x| match x {
+    0 => "no more bottles of beer".to_string(),
+    1 => "1 bottle of beer".to_string(),
+    _ => format!("{} bottles of beer", x),
+  };
+  let mut result = format!("{} on the wall, {}.\n", capitalized(b(number)), b(number));
+
+  result.push_str(
+    &match number {
+      0 => "Go to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+      1 => "Take it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+      _ => format!("Take one down and pass it around, {} on the wall.\n", b(number - 1)),
+    }
+  );
+  result
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    let mut str: String = String::new();
-    for n in (end..(start + 1)).rev() {
-      str += &verse(n);
-      if n != end {
-        str += "\n";
-      }
-    }
-    str
+  (end..=start).rev().map(verse).collect::<Vec<String>>().join("\n")
 }
